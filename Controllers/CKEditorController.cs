e7ad12e9420791c3d99ab253409ee3225d89926e
sumanth.jalli <sumanth.jalli@gmail.com>
2019-01-31 12:48:34 +0530

    initial code commit.

---
+++ Controllers/CKEditorController.cs
@@ -0,0 +1,32 @@
+﻿using CKEditorDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CKEditorDemo.Controllers
+{
+    public class CKEditorController : Controller
+    {
+        // GET: CKEditor
+        CKEditorDemoDBEntities context = new CKEditorDemoDBEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Index(TestModel mdl)
+        {
+            Test tstobj = new Test();
+            tstobj.Title = mdl.Title;
+            tstobj.HelpContent = mdl.HelpContent;
+            context.Tests.Add(tstobj);
+            context.SaveChanges();
+
+            return View();
+        }
+    }
+}
